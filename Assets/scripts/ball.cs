e7f7234c9b051211abc30272bf3a1a178887ba8f
LoekLucas <l.van.kessel@sintlucasedu.nl>
2023-09-29 12:50:08 +0200

    Added speed and position variables and added starts for collision logic

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -1,9 +1,15 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ball : MonoBehaviour
 {
+    public float xPosition = 2f;
+    public float yPosition = 2f;
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,9 @@ public class ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        xPosition = xPosition + xSpeed * Time.deltaTime;
+        yPosition = yPosition + ySpeed * Time.deltaTime;
+        transform.position = new Vector3(xPosition, yPosition, 0f);  
     }
 }
