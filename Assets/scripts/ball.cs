0c43ce09ba83e3b1751565130d245c7dddaaccd1
LoekLucas <l.van.kessel@sintlucasedu.nl>
2023-09-29 12:14:46 +0200

    Initial commit

---
+++ Assets/scripts/ball.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(2f,2f,0f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
