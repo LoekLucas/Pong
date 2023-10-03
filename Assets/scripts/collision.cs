8ed370b1f9f2d9620c3962359d502f687f4fcc1f
LoekLucas <l.van.kessel@sintlucasedu.nl>
2023-10-03 14:14:57 +0200

    Added scenes etc

---
+++ Assets/scripts/collision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public float xPosition = 2f;
+    public float yPosition = 2f;
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(2f, 2f, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xPosition = xPosition + xSpeed * Time.deltaTime;
+        yPosition = yPosition + ySpeed * Time.deltaTime;
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+}
